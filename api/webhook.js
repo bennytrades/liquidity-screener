2c66cee0c26541f69e92913d271b004eeec1df43
Trime <benhinkley@protonmail.com>
2025-05-13 22:35:28 +0930

    Firestore test

--- api/webhook.js
+++ api/webhook.js
@@ -1,8 +1,8 @@
 import admin from 'firebase-admin';
 
-const serviceAccount = require('../../apitoken.json');
-
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
