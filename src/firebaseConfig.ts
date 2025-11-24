e517d70246dcebebfc8cee0507884ab2f067fd45
Trime <benhinkley@protonmail.com>
2025-11-24 21:55:00 +1030

    Add real Firebase credentials

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_REACT_APP_FIREBASE_API_KEY_VALUE",
-  authDomain: "PASTE_YOUR_REACT_APP_FIREBASE_AUTH_DOMAIN_VALUE",
-  projectId: "PASTE_YOUR_REACT_APP_FIREBASE_PROJECT_ID_VALUE",
-  storageBucket: "PASTE_YOUR_REACT_APP_FIREBASE_STORAGE_BUCKET_VALUE",
-  messagingSenderId: "PASTE_YOUR_REACT_APP_FIREBASE_MESSAGING_SENDER_ID_VALUE",
-  appId: "PASTE_YOUR_REACT_APP_FIREBASE_APP_ID_VALUE",
+  apiKey: "AIzaSyCGeGDcQ1zVbtQVFYkHfSue5OIIPuIcS6Y",
+  authDomain: "screener-e8718.firebaseapp.com",
+  projectId: "screener-e8718",
+  storageBucket: "screener-e8718.appspot.com",
+  messagingSenderId: "190387003372",
+  appId: "1:190387003372:web:32cbd278708199a96aaac9",
 };
 
 const app = initializeApp(firebaseConfig);
